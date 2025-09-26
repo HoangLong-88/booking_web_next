8e881ba6627b64fdfc9d6e0319f96ffeb3452366
HoangLong-88 <quoccuong130406@gmail.com>
2025-09-26 20:34:41 +0700

    upload feature

--- src/app/flights/page.tsx
+++ src/app/flights/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 
-export default function HomePage() {
+export default function Home() {
   return (
     <div className="font-bold font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      HELLO NEXT!
+      Flights Booking Page!
     </div>
   );
 }
