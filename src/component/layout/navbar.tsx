cf7a06dda6a4fc7a60393c714c67c1b63eb81015
XxDdUu <tranquangduy15122006@gmail.com>
2025-10-10 21:41:08 +0700

    merge homepage & navbar

--- src/component/layout/navbar.tsx
+++ src/component/layout/navbar.tsx
@@ -2,26 +2,37 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
-// import {useAuthStore} from "@/";
+import { LanguageSelector } from "./menu_selector/language_selector";
 
 const Nav = [
     {name: 'Stays',href: '/stays', }
 ]
 
-export function NavBar() {
+const navItems = [
+  { name: "Stays", href: "/stays" },
+  { name: "FlighSign Ints", href: "/flights" },
+  { name: "Cars", href: "/cars" },
+  { name: "Attractions", href: "/atractions" },
+];
+
+function NavBar() {
     const pathname = usePathname()
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
     // const [isAuthenticated, user, logout, fetchCurrentUser] = useAuthStore()
     // const [mounted, setMounted] = useState(false)
 
     return (
-        <nav className="bg-blue-300 text-black px-10 py-3 flex justify-between items-center">
+        <nav className="h-[6.5rem] px-4 py-3 px-[2.5rem] flex justify-between
+         items-center" style={{ background: "var(--navbar-background)" }}>
             {/* Logo */}
-            <div className="font-bold text-3xl mr-7">
-                <Link href="/">SKYLINK</Link>
-            </div> 
+            <div>
+                <div className="font-bold text-5xl text-amber-50">
+                    <Link href="/">SKYLINK</Link>
+                </div>
+                <div className="text-1xl text-amber-50">Your travel partner</div> 
+            </div>
 
-            {/* Menu button (mobile) */}
+            {/* Menu button (mobile) */}    
             <button
                 className="md:hidden"
                 onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
@@ -31,35 +42,41 @@ export function NavBar() {
 
             {/* Menu items */}
             <ul
-                className={`ml-20 md:flex md:space-x-6 absolute md:static left-0 w-full md:w-auto bg-blue-600 
-                    md:bg-transparent transition-all ${mobileMenuOpen ? "top-12" : "-top-60"}`}
+                className={`md:flex md:space-x-2 md:static  
+                    absolute 
+                    md:w-auto bg-(#C9AE85)
+                    md:bg-transparent 
+                    text-[#7e644e]
+                    text-1xl 
+                    transition-all ${mobileMenuOpen ? "top-12" : "-top-60"}`}
             >
-                <li className="p-2 md:p-0">
-                    <Link href="/stays">Stays</Link>
-                </li>
-                <li className="p-2 md:p-0">
-                    <Link href="/flights">Flights</Link>
-                </li>
-                <li className="p-2 md:p-0">
-                    <Link href="/cars">Cars</Link>
-                </li>
-                <li className="p-2 md:p-0">
-                    <Link href="/atractions">Attractions</Link>
-                </li>
-                <li className="p-2 md:p-0">
-                    <Link href="/taxis">Taxis</Link>
-                </li>
+                {navItems.map((item) => {
+                    const isActive = pathname === item.href;
+
+                    return (
+                    <li key={item.name}>
+                        <Link
+                            href={item.href}
+                            className={`py-2 px-3 rounded-2xl font-semibold transition-colors duration-200 ${
+                            isActive
+                                ? `bg-transparent
+                                     text-gray-100 border-1`
+                                : "text-gray-200 hover:bg-neutral-400"
+                            }`} 
+                        >
+                            {item.name}
+                        </Link>
+                        </li>
+                    );
+                })}
             </ul>
 
             <div className={`md:flex md:space-x-6 absolute md:static left-0 w-full md:w-auto bg-blue-600 md:bg-transparent transition-all ${mobileMenuOpen ? "top-12" : "-top-60"
                     }`}>
-                <button className="rounded-xl">
-                        VND
-                </button>
-                <div>🇻🇳</div>
-                <div>Sign Up</div>
-                <div>Sign In</div>
+                <LanguageSelector />
+                <div><img src="/icon/auth_icon/user_icon.png" alt="Logo" className="w-10" /></div>
             </div>
         </nav>
     );
 }
+export { NavBar };
