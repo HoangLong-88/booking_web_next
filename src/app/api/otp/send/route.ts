e50841621aee1f969b5280bef484b17b282ffa66
XxDdUu <tranquangduy15122006@gmail.com>
2025-11-28 21:28:51 +0700

    log: test log

--- src/app/api/otp/send/route.ts
+++ src/app/api/otp/send/route.ts
@@ -1,6 +1,7 @@
 export async function POST(req: Request) {
   const { contact } = await req.json();
-
+  const body = await req.json();
+  console.log("Next.js API Route hit!", body);
   const laravelRes = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/api/otp/send-otp`,
     {
@@ -17,7 +18,7 @@ export async function POST(req: Request) {
     data = { success: false, message: text || "Empty response from Laravel" };
   }
   // const data = await laravelRes.json();
-
+  console.log("Response from Laravel:", data); // <-- log response
   return Response.json(data, {
     status: laravelRes.status
   });
